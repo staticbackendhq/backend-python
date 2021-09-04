608998c79b18efe734a56039ec173b20c6e5c494
ipfans <363344+ipfans@users.noreply.github.com>
2021-09-04 21:25:36 +0800

    fix: Make CI happy

--- tests/test_staticbackend.py
+++ tests/test_staticbackend.py
@@ -1,8 +1,5 @@
 import unittest
 
-from pydantic import ValidationError
-from staticbackend import Config, StaticBackend
-
 
 class TestStaticBackend(unittest.TestCase):
     def test_login(self) -> None:
