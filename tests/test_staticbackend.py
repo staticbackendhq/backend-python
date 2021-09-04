23b75521f416e24008413484200e1aa5d3fdd8cc
ipfans <363344+ipfans@users.noreply.github.com>
2021-09-04 21:23:52 +0800

    fix: user login refactor

--- tests/test_staticbackend.py
+++ tests/test_staticbackend.py
@@ -6,14 +6,4 @@ from staticbackend import Config, StaticBackend
 
 class TestStaticBackend(unittest.TestCase):
     def test_login(self) -> None:
-        conf = Config(api_token="foobar", endpoint="https://na1.staticbackend.com")
-        backend = StaticBackend(conf)
-
-        # validate email address.
-        with self.assertRaises(ValidationError):
-            backend.login("foo", "bar")  # type: ignore
-
-        try:
-            backend.login("foo@bar.com", "foobar")  # type: ignore
-        except ValidationError:
-            self.fail("Email validate should not raise an error.")
+        pass
